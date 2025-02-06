ea42cbf1d8213e8420fc153a385ab016702f6cc0
italiano@oplabs.co <italiano@oplabs.co>
2025-02-05 19:34:53 -0500

    Remove unused package utils

--- src/packageUtils.ts
+++ src/packageUtils.ts
@@ -1,5 +1,4 @@
 import { getPackages, Package } from '@manypkg/get-packages';
-import { ExecCommandResult } from './gitUtils';
 
 export async function getVersionsByDirectory(cwd: string) {
     const { packages } = await getPackages(cwd);
@@ -21,76 +20,3 @@ export async function getChangedPackages(
 
     return [...changedPackages];
 }
-
-export type PublishOptions = {
-    changesetPublishOutput: ExecCommandResult;
-    cwd?: string;
-}
-
-export type PublishResult = {
-    published: boolean;
-    publishedPackages: {
-        name: string;
-        version: string;
-    }[];
-}
-
-export async function getReleasedPackages({
-    changesetPublishOutput,
-    cwd = process.cwd(),
-  }: PublishOptions): Promise<PublishResult> {
-    console.log('getReleasedPackages', changesetPublishOutput.stdout);
-    const { packages, tool } = await getPackages(cwd);
-    const releasedPackages: Package[] = [];
-  
-    if (tool.type !== "root") {
-      const newTagRegex = /New tag:\s+(@[^/]+\/[^@]+|[^/]+)@([^\s]+)/;
-      const packagesByName = new Map(packages.map((x) => [x.packageJson.name, x]));
-  
-      for (let line of changesetPublishOutput.stdout.split("\n")) {
-        const match = line.match(newTagRegex);
-        if (match === null) {
-          continue;
-        }
-        const pkgName = match[1];
-        const pkg = packagesByName.get(pkgName);
-        if (pkg === undefined) {
-          throw new Error(
-            `Package "${pkgName}" not found.` +
-              "This is probably a bug in the action, please open an issue"
-          );
-        }
-        releasedPackages.push(pkg);
-      }
-    } else {
-      if (packages.length === 0) {
-        throw new Error(
-          `No package found.` +
-            "This is probably a bug in the action, please open an issue"
-        );
-      }
-      const pkg = packages[0];
-      const newTagRegex = /New tag:/;
-  
-      for (let line of changesetPublishOutput.stdout.split("\n")) {
-        const match = line.match(newTagRegex);
-  
-        if (match) {
-          releasedPackages.push(pkg);
-          break;
-        }
-      }
-    }
-  
-    if (releasedPackages.length) {
-      return {
-        published: true,
-        publishedPackages: releasedPackages.map((pkg) => ({
-          name: pkg.packageJson.name,
-          version: pkg.packageJson.version,
-        })),
-      };
-    }
-  
-    return { published: false, publishedPackages: [] };
-  }
